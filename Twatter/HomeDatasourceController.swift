08dd9db5198b107f4f881a64669d88288864e7a8
Stanley Pan <stanleypan.egre@gmail.com>
2017-01-03 17:58:49 +0900

    Render out main cells using new framework

--- Twatter/HomeDatasourceController.swift
+++ Twatter/HomeDatasourceController.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2017 Stanley Pan. All rights reserved.
 //
 
-import Foundation
+import LBTAComponents
+
+class UserCell: DatasourceCell {
+    
+    override func setupViews() {
+        super.setupViews()
+        backgroundColor = .yellow
+    }
+}
+
+class HomeDatasource: Datasource {
+    
+    let words = ["user1", "user2", "user3"]
+    
+    override func cellClasses() -> [DatasourceCell.Type] {
+        return [UserCell.self]
+    }
+    
+    override func item(_ indexPath: IndexPath) -> Any? {
+        return words[indexPath.item]
+    }
+    
+    override func numberOfItems(_ section: Int) -> Int {
+        return words.count
+    }
+}
+
+class HomeDatasourceController: DatasourceController {
+    // MARK: viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeDatasource = HomeDatasource()
+        self.datasource = homeDatasource
+    }
+}

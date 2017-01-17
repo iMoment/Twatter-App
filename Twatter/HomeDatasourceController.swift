a1db162f03d29f810913c5136d1cdea352a30d55
Stanley Pan <stanleypan.egre@gmail.com>
2017-01-17 17:38:20 +0900

    Refactor nav bar code

--- Twatter/HomeDatasourceController.swift
+++ Twatter/HomeDatasourceController.swift
@@ -20,30 +20,6 @@ class HomeDatasourceController: DatasourceController {
         self.datasource = homeDatasource
     }
     
-    private func setupNavigationBarItems() {
-        
-        let titleImageView = UIImageView(image: #imageLiteral(resourceName: "title_icon"))
-        titleImageView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-        titleImageView.contentMode = .scaleAspectFit
-        
-        navigationItem.titleView = titleImageView
-        
-        let followButton = UIButton(type: .system)
-        followButton.setImage(#imageLiteral(resourceName: "follow").withRenderingMode(.alwaysOriginal), for: .normal)
-        followButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
-        
-        let searchButton = UIButton(type: .system)
-        searchButton.setImage(#imageLiteral(resourceName: "search").withRenderingMode(.alwaysOriginal), for: .normal)
-        searchButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-        
-        let composeButton = UIButton(type: .system)
-        composeButton.setImage(#imageLiteral(resourceName: "compose").withRenderingMode(.alwaysOriginal), for: .normal)
-        composeButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-        // Multiple bar button items need to be specified
-        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: composeButton), UIBarButtonItem(customView: searchButton)]
-    }
-    
     // MARK: Gap between cells
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         
